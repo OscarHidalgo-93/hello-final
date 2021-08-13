baf7de3c2714b8b331d5101c13b8d9d80fcfce58
Oscar Hidalgo Alvarez <oscarhidalgo-93@hotmail.com>
2021-08-13 13:56:43 +0200

    Editados docker-compose,dummy-test

--- src/test/java/com/viewnext/hellofinal/DumyTest.java
+++ src/test/java/com/viewnext/hellofinal/DumyTest.java
@@ -7,9 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DumyTest {
-
+public class DumyTest {
     @LocalServerPort
     private int port;
 
